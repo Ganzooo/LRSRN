d0230e970558872c4ebe7a9d2d5118ba6cf9ac4a
Ganzoo <gnzrg25@gmail.com>
2023-03-14 13:00:37 +0900

    -- fine tune tset

--- script/script_train_A100_gpu3.sh
+++ script/script_train_A100_gpu3.sh
@@ -1,2 +1,2 @@
 cd ../
-python train.py --config ./configs/repConv/A100/repConv_x3_m6c64_relu_div2k_multi_lr5e-4.yml  --gpu_ids 3
+python train.py --config ./configs/repConv/A100/repConv_x3_m6c64_gelu_div2k_multi_lr5e-4_fine_tune.yml  --gpu_ids 3
