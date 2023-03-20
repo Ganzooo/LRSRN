ea969c41ca87adeefedd16979fb4a7eb62cd1564
Ganzoo <gnzrg25@gmail.com>
2023-03-20 22:45:36 +0900

    .

---
+++ script/script_train_A100_gpu3_1.sh
@@ -0,0 +1,2 @@
+cd ../
+python train.py --config ./configs/repConv/A100/repConvV2_x3_m4c48_relu_div2kA_warmup_lr5e-4_b8_p384_normalize_ft_combined2.yml  --gpu_ids 3
--- script/script_train_A100_gpu3_C.sh
+++
@@ -1,2 +0,0 @@
-cd ../
-python train.py --config ./configs/repConv/A100/repConv_x3_m4c64_relu_combined3_warmup_lr5e-4_fine_tune.yml  --gpu_ids 3
