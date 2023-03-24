fe685e79cb26a6f4e8649c4727ad7c26b0f3ef79
micro02 <rlghksdbs@gmail.com>
2023-03-24 22:02:49 +0900

    upload file for ablation study

---
+++ script/Ablation_train_X2_plain_m4c32p192.sh
@@ -0,0 +1,2 @@
+cd ../
+python train.py --config ./configs/cvprw2023/ablation/plainsr_x2_m4c32_192_relu.yml  --gpu_ids 0
---
+++ script/Ablation_train_X2_repconvV2_m4c32p384 .sh
@@ -0,0 +1,2 @@
+cd ../
+python train.py --config ./configs/cvprw2023/ablation/repConvV2_x2_m4c32_384_relu.yml  --gpu_ids 3
